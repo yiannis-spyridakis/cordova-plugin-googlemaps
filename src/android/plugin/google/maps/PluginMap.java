a6346519a0526faf4e305fcd4fe3cce7524b03ba
yiannis-spyridakis <yiannis.spyridakis@logismix.com>
2024-04-04 11:07:21 +0300

    PluginMap.java -> Fixed double plugin initialization => Maps not showing on Android

--- src/android/plugin/google/maps/PluginMap.java
+++ src/android/plugin/google/maps/PluginMap.java
@@ -631,9 +631,9 @@ public class PluginMap extends MyPlugin implements OnMarkerClickListener,
       plugins.put(pluginName, pluginEntry);
       mapCtrl.pluginManager.addService(pluginEntry);
 
-      plugin.privateInitialize(pluginName, cordova, webView, null);
+      // plugin.privateInitialize(pluginName, cordova, webView, null);
 
-      plugin.initialize(cordova, webView);
+      // plugin.initialize(cordova, webView);
       ((MyPluginInterface)plugin).setPluginMap(PluginMap.this);
       MyPlugin myPlugin = (MyPlugin) plugin;
       myPlugin.self = (MyPlugin)plugin;
@@ -684,8 +684,8 @@ public class PluginMap extends MyPlugin implements OnMarkerClickListener,
       pluginMap = PluginMap.this;
       pluginMap.mapCtrl.pluginManager.addService(pluginEntry);
 
-      plugin.privateInitialize(className, cordova, webView, null);
-      plugin.initialize(cordova, webView);
+      // plugin.privateInitialize(className, cordova, webView, null);
+      // plugin.initialize(cordova, webView);
       ((MyPluginInterface)plugin).setPluginMap(PluginMap.this);
       pluginEntry.plugin.execute("create", args, callbackContext);
 
